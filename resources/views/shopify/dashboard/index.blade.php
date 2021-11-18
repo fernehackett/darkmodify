541d262b05cda226ab071383ac9dc6476d1ebc07
Nic <tmninh94@gmail.com>
2021-11-18 11:14:52 +0700

    "Nic"

--- resources/views/shopify/dashboard/index.blade.php
+++ resources/views/shopify/dashboard/index.blade.php
@@ -30,12 +30,12 @@
         <div @class(["schedule","d-none"=> !$store || $store->schedule == 0])>
             <div class="form-group">
                 <label>Begin At</label>
-                <input type="time" name="begin_at" class="form-control" value="{{ $store->begin_at ?? "" }}">
+                <input type="time" name="begin_at" class="form-control" value="{{ $store->begin_at ? $store->begin_at->format("H:i") : "" }}">
             </div>
 
             <div class="form-group">
                 <label>End At</label>
-                <input type="time" name="end_at" class="form-control" value="{{ $store->end_at ?? "" }}">
+                <input type="time" name="end_at" class="form-control" value="{{ $store->end_at  ? $store->end_at->format("H:i") : "" }}">
             </div>
         </div>
         <div class="form-group">
