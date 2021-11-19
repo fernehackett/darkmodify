38d2092947413ef60e1ea3c829c606cebc4ce69b
Nic <tmninh94@gmail.com>
2021-11-19 07:39:44 +0700

    "Nic"

--- resources/views/shopify/instructions/index.blade.php
+++ resources/views/shopify/instructions/index.blade.php
@@ -15,9 +15,9 @@
                     </dd>
                     <dt>Step 2</dt>
                     <dd>
-                        Click to <i>Enable DarkModify</i> to turn on the dark mode.
-                        Click to <i>Enable Schedule </i> to turn on the schedule. You must choose the begin and end time to turn on the dark mode.
-                        Click to <i>Customer can turn on/off</i> if you want the visitor can be turn on or turn off.
+                        Click to <i>Enable DarkModify</i> to turn on the dark mode.<br>
+                        Click to <i>Enable Schedule </i> to turn on the schedule. You must choose the begin and end time to turn on the dark mode.<br>
+                        Click to <i>Customer can turn on/off</i> if you want the visitor can be turn on or turn off.<br>
                     </dd>
                     <dt>Step 3</dt>
                     <dd>
@@ -25,15 +25,15 @@
                     </dd>
                     <dt>Step 4</dt>
                     <dd>
-                        If you enable <i>Customer can turn on/off</i>, you need go to <b>Online Store > Themes > Customizes</b>.
-                        Go to <b>Theme settings</b> then click to <b>App embeds</b> tab.
-                        Click to <b>Dark Mode Toggle</b>, you will see some options.
-                        1. Width: The width of widget of storefront
-                        2. Top: The space to top of storefront
-                        3. Bottom: The space to bottom of storefront
-                        4. Left: The space to left of storefront
-                        5. Right: The space to right of storefront
-                        6. Index: The index of storefront
+                        If you enable <i>Customer can turn on/off</i>, you need go to <b>Online Store > Themes > Customizes</b>.<br>
+                        Go to <b>Theme settings</b> then click to <b>App embeds</b> tab.<br>
+                        Click to <b>Dark Mode Toggle</b>, you will see some options.<br>
+                        1. Width: The width of widget of storefront.<br>
+                        2. Top: The space to top of storefront.<br>
+                        3. Bottom: The space to bottom of storefront.<br>
+                        4. Left: The space to left of storefront.<br>
+                        5. Right: The space to right of storefront.<br>
+                        6. Index: The index of storefront.<br>
                     </dd>
                 </dl>
             </div>
