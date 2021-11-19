96ed6c94c7b6481908d36b9a4f5654791be3c564
Nic <tmninh94@gmail.com>
2021-11-19 07:23:33 +0700

    "Nic"

--- resources/views/shopify/script-tags/index.blade.php
+++ resources/views/shopify/script-tags/index.blade.php
@@ -46,6 +46,7 @@ if(toggle){
 @endif
 @if($store->schedule == 1 && $store->begin_at && $store->end_at)
     let begin_at = new Date(`{{ $store->begin_at }}`);
+    if(current < begin_at)  begin_at.setDate(begin_at.getDate() - 1)
     let end_at = new Date(`{{ $store->end_at }}`);
     if(end_at < begin_at) end_at.setDate(end_at.getDate() + 1)
     let current = new Date();
