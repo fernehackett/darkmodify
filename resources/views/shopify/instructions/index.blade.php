a9781ba8cde02723ad7822e127118a2f4cd4e65c
Nic <tmninh94@gmail.com>
2021-11-19 07:44:42 +0700

    "Nic"

--- resources/views/shopify/instructions/index.blade.php
+++ resources/views/shopify/instructions/index.blade.php
@@ -11,13 +11,13 @@
                 <dl>
                     <dt>Step 1</dt>
                     <dd>
-                        After install <b>DarkModify</b>, you will the options page.
+                        After install <b>DarkModify</b>, you will go to the set up page.
                     </dd>
                     <dt>Step 2</dt>
                     <dd>
                         Click to <i>Enable DarkModify</i> to turn on the dark mode.<br>
                         Click to <i>Enable Schedule </i> to turn on the schedule. You must choose the begin and end time to turn on the dark mode.<br>
-                        Click to <i>Customer can turn on/off</i> if you want the visitor can be turn on or turn off.<br>
+                        Click to <i>Customer can turn on/off</i> if you want the visitor can be turn on or turn off. You must enable the switcher in the Theme settings<br>
                     </dd>
                     <dt>Step 3</dt>
                     <dd>
@@ -29,11 +29,11 @@
                         Go to <b>Theme settings</b> then click to <b>App embeds</b> tab.<br>
                         Click to <b>Dark Mode Toggle</b>, you will see some options.<br>
                         1. Width: The width of widget of storefront.<br>
-                        2. Top: The space to top of storefront.<br>
-                        3. Bottom: The space to bottom of storefront.<br>
-                        4. Left: The space to left of storefront.<br>
-                        5. Right: The space to right of storefront.<br>
-                        6. Index: The index of storefront.<br>
+                        2. Top: The space from top of storefront to the widget.<br>
+                        3. Bottom: The space from bottom of storefront to the widget.<br>
+                        4. Left: The space from left of storefront to the widget.<br>
+                        5. Right: The space from right  of storefront to the widget.<br>
+                        6. Index: The priotity index of widget in storefront.<br>
                     </dd>
                 </dl>
             </div>
